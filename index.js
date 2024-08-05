4c4cacb905404dc73fb2675b4d8319530c759ef9
Itumeleng013 <itumodisane13@gmail.com>
2024-08-05 11:24:19 +0200

    API endpoint
    added api endpoint to return request header information

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// API endpoint to return request header information
+app.get('api/whoami', (req,res)=>{
+  const ipaddress = req.ip || req.headers['x-fowarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  //send the information to a json response
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
